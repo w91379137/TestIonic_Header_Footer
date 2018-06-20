5305c0f4777d701f1b58ceea7ab8d4a573ace905
林永璋 <yclin@jumbogames.com.tw>
2018-06-20 11:58:26 +0800

    增加生命週期

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
+export class AboutPage implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterContentChecked, OnDestroy {
 
   dataArray: DataBlock[] = []
 
@@ -16,6 +16,67 @@ export class AboutPage {
     this.dataArray = category.map(number => new DataBlock(number.toString(), Array.from({ length: 10 }, (_, i) => (i + number * 10).toString())))
   }
 
+  //Lifecycle ionic
+  //https://blog.ionicframework.com/navigating-lifecycle-events/
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad")
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter")
+  }
+
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter")
+  }
+
+  ionViewWillLeave() {
+    console.log("ionViewWillLeave")
+  }
+
+  ionViewDidLeave() {
+    console.log("ionViewDidLeave")
+  }
+
+  ionViewWillUnload() {
+    console.log("ionViewWillUnload")
+  }
+
+  //Lifecycle Angular
+  ngOnChanges() {
+    console.log("ngOnChanges")
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit")
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck")
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit")
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked")
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit")
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked")
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy")
+  }
+
+  //Action
   buttonClickAction(event) {
     console.log(event.target)
   }
